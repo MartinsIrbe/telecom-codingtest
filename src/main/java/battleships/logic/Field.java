08b25c898f4130472032f07accf3cc7825be119b
Martins <martins.git@irbe.co.uk>
2017-04-30 14:44:52 +0100

    Refactoring the game logic to store 2-d array for ships instead of integers as this makes easier to determine if the ship has sunk or not. Created ship interface that are extended by battleship and destroyer classes to make it easier to store each ship individual attribute, such as name, the ship length and coordinates on the field. Once all ship coordinates have been destroyed, the ship is sunk.

--- src/main/java/battleships/logic/Field.java
+++ src/main/java/battleships/logic/Field.java
@@ -1,5 +1,8 @@
-package battleships;
+package battleships.logic;
 
+import battleships.logic.ship.Battleship;
+import battleships.logic.ship.Destroyer;
+import battleships.logic.ship.Ship;
 import com.google.common.collect.Maps;
 
 import java.util.Map;
@@ -11,8 +14,6 @@ public class Field {
 
     private static final String BATTLESHIP_ID = "Battleship";
     private static final String DESTROYER_ID = "Destroyers";
-    private static final int BATTLESHIP_LENGTH = 5;
-    private static final int DESTROYER_LENGTH = 4;
     private static final int FIELD_SIZE = 10;
 
     private static final int DIRECTION_LEFT = 0;
@@ -22,76 +23,84 @@ public class Field {
 
     private Map<String, Integer> ships;
     private Random random;
-    private int[][] field;
+    private Ship[][] field;
+
+    private int totalShips = 0;
 
     public Field(Random random, int battleshipsCount, int destroyersCount) {
         this.random = checkNotNull(random);
 
-        this.field = new int[FIELD_SIZE][FIELD_SIZE];
+        this.field = new Ship[FIELD_SIZE][FIELD_SIZE];
         this.ships = Maps.newHashMap();
         ships.put(BATTLESHIP_ID, battleshipsCount);
         ships.put(DESTROYER_ID, destroyersCount);
     }
 
-    public int[][] generateNewField() {
+    public Ship[][] generateNewField() {
         for (Map.Entry<String, Integer> ship : ships.entrySet()) {
             if (ship.getKey().equals(BATTLESHIP_ID)) {
-                placeShip(BATTLESHIP_LENGTH);
+                placeShip(new Battleship());
+                totalShips++;
             } else if (ship.getKey().equals(DESTROYER_ID)) {
-                placeShip(DESTROYER_LENGTH);
+                placeShip(new Destroyer());
+                totalShips++;
             }
         }
         return field;
     }
 
-    public void placeShip(int battleShipSize) {
+    public void placeShip(Ship ship) {
         boolean shipNotPlaced = true;
         while (shipNotPlaced) {
             int rowStart = random.nextInt(FIELD_SIZE);
             int columnStart = random.nextInt(FIELD_SIZE);
-            if (field[rowStart][columnStart] == 0) {
+            if (field[rowStart][columnStart] == null) {
                 int direction = random.nextInt(4);
-                shipNotPlaced = !attemptToPlaceShip(battleShipSize, rowStart, columnStart, direction);
+                shipNotPlaced = !attemptToPlaceShip(ship, rowStart, columnStart, direction);
             }
         }
     }
 
     private boolean attemptToPlaceShip(
-            int battleShipSize,
+            Ship ship,
             int rowStart,
             int columnStart,
             int direction
     ) {
         boolean validPlacement = false;
         if (direction == DIRECTION_LEFT) {
-            int rowEnd = rowStart + (battleShipSize * -1) + 1;
+            int rowEnd = rowStart + (ship.getLength() * -1) + 1;
             if (isWithinBounds(rowEnd) && isValidPlacement(rowStart, rowEnd, columnStart, columnStart)) {
                 for (int rowIndex = rowEnd; rowIndex <= rowStart; rowIndex++) {
-                    field[rowIndex][columnStart] = battleShipSize;
+                    field[rowIndex][columnStart] = ship;
+                    ship.addCoordinates(rowIndex, columnStart);
                 }
                 validPlacement = true;
             }
         } else if (direction == DIRECTION_RIGHT) {
-            int rowEnd = rowStart + battleShipSize - 1;
+            int rowEnd = rowStart + ship.getLength() - 1;
             if (isWithinBounds(rowEnd) && rowEnd >= 0 && isValidPlacement(rowEnd, rowStart, columnStart, columnStart)) {
                 for (int rowIndex = rowStart; rowIndex <= rowEnd; rowIndex++) {
-                    field[rowIndex][columnStart] = battleShipSize;
+                    field[rowIndex][columnStart] = ship;
+                    ship.addCoordinates(rowIndex, columnStart);
                 }
                 validPlacement = true;
             }
         } else if (direction == DIRECTION_UP) {
-            int columnEnd = columnStart + (battleShipSize * -1) + 1;
+            int columnEnd = columnStart + (ship.getLength() * -1) + 1;
             if (isWithinBounds(columnEnd) && isValidPlacement(rowStart, rowStart, columnEnd, columnStart)) {
                 for (int columnIndex = columnEnd; columnIndex <= columnStart; columnIndex++) {
-                    field[rowStart][columnIndex] = battleShipSize;
+                    field[rowStart][columnIndex] = ship;
+                    ship.addCoordinates(rowStart, columnIndex);
                 }
                 validPlacement = true;
             }
         } else if (direction == DIRECTION_DOWN) {
-            int columnEnd = columnStart + battleShipSize - 1;
+            int columnEnd = columnStart + ship.getLength() - 1;
             if (isWithinBounds(columnEnd) && isValidPlacement(rowStart, rowStart, columnStart, columnEnd)) {
                 for (int columnIndex = columnStart; columnIndex <= columnEnd; columnIndex++) {
-                    field[rowStart][columnIndex] = battleShipSize;
+                    field[rowStart][columnIndex] = ship;
+                    ship.addCoordinates(rowStart, columnIndex);
                 }
                 validPlacement = true;
             }
@@ -106,13 +115,13 @@ public class Field {
     public boolean isValidPlacement(int rowStart, int rowEnd, int columnStart, int columnEnd) {
         if (columnStart == columnEnd) {
             for (int rowIndex = rowEnd; rowIndex <= rowStart; rowIndex++) {
-                if (field[rowIndex][columnStart] != 0) {
+                if (field[rowIndex][columnStart] != null) {
                     return false;
                 }
             }
         } else if (rowStart == rowEnd) {
             for (int columnIndex = columnEnd; columnIndex <= columnStart; columnIndex++) {
-                if (field[rowStart][columnIndex] != 0) {
+                if (field[rowStart][columnIndex] != null) {
                     return false;
                 }
             }
@@ -123,9 +132,41 @@ public class Field {
         return true;
     }
 
-    public boolean attack(String coordinates) {
-        // TODO : Implement logic for executing an attack. Return true/false if the attack has been successful.
-        return false;
+    public boolean attack(int[] coordinates) {
+        int rowIndex = coordinates[0];
+        int columnIndex = coordinates[1];
+        boolean attackSuccessful = false;
+        Ship ship = field[rowIndex][columnIndex];
+        if (ship != null && ship.validCoordinates(rowIndex, columnIndex)) {
+            ship.attack(rowIndex, columnIndex);
+            totalShips--;
+            return true;
+        }
+
+        return attackSuccessful;
+    }
+
+    public boolean checkIfShipSunk(int[] coordinates) {
+        int rowIndex = coordinates[0];
+        int columnIndex = coordinates[1];
+
+        return field[rowIndex][columnIndex].isSunk();
+    }
+
+    /**
+     * Used to convert coordinates string to int array
+     * that holds coordinate indexes for row and column.
+     * @param coordinates String that contains coordinates.
+     * @return An array where first element is row index, second is column index.
+     */
+    public int[] getCoordinates(String coordinates) {
+        String[] coordinatesParts = new String[]{coordinates.substring(0, 1), coordinates.substring(1)};
+
+        int[] extractedCoordinates = new int[2];
+        extractedCoordinates[0] = Integer.parseInt(coordinatesParts[1]) - 1;
+        extractedCoordinates[1] = coordinatesParts[0].toLowerCase().charAt(0) - 'a';
+
+        return extractedCoordinates;
     }
 
     public void printBattlefield() {
@@ -133,11 +174,10 @@ public class Field {
     }
 
     public boolean battleIsOver() {
-        // TODO : Returns true if the battle is over and all ships have been destroyed.
-        return false;
+        return totalShips == 0;
     }
 
-    public int[][] getField() {
+    public Ship[][] getField() {
         return field;
     }
 }
--- src/test/java/battleships/FieldTest.java
+++ src/test/java/battleships/FieldTest.java
@@ -1,5 +1,9 @@
 package battleships;
 
+import battleships.logic.Field;
+import battleships.logic.ship.Battleship;
+import battleships.logic.ship.Destroyer;
+import battleships.logic.ship.Ship;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +13,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
@@ -19,15 +24,19 @@ public class FieldTest {
     @Mock
     private Random random;
 
-    private int fieldSize = 10;
-    private int battleshipsCount;
-    private int destroyersCount;
+    private static final String BATTLESHIP_ID = "Battleship";
+    private static final int BATTLESHIP_COUNT = 1;
+    private static final int BATTLESHIP_LENGTH = 5;
+
+    private static final String DESTROYER_ID = "Destroyers";
+    private static final int DESTROYERS_COUNT = 2;
+    private static final int DESTROYER_LENGTH = 4;
 
     private Field field;
 
     @Before
     public void setUp() throws Exception {
-        this.field = new Field(random, battleshipsCount, destroyersCount);
+        this.field = new Field(random, BATTLESHIP_COUNT, DESTROYERS_COUNT);
     }
 
     @Test
@@ -35,14 +44,15 @@ public class FieldTest {
         when(random.nextInt(10)).thenReturn(4, 0);
         when(random.nextInt(4)).thenReturn(0);
 
-        field.placeShip(5);
+        Battleship battleship = new Battleship();
+        field.placeShip(battleship);
 
-        int[][] battlefield = field.getField();
-        assertThat(battlefield[0][0], is(5));
-        assertThat(battlefield[1][0], is(5));
-        assertThat(battlefield[2][0], is(5));
-        assertThat(battlefield[3][0], is(5));
-        assertThat(battlefield[4][0], is(5));
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[0][0].equals(battleship));
+        assertTrue(battlefield[1][0].equals(battleship));
+        assertTrue(battlefield[2][0].equals(battleship));
+        assertTrue(battlefield[3][0].equals(battleship));
+        assertTrue(battlefield[4][0].equals(battleship));
     }
 
     @Test
@@ -50,11 +60,44 @@ public class FieldTest {
         when(random.nextInt(10)).thenReturn(3, 0, 4, 1);
         when(random.nextInt(4)).thenReturn(0);
 
-        field.placeShip(5);
+        field.placeShip(new Battleship());
 
-        int[][] battlefield = field.getField();
-        assertThat(battlefield[0][0], is(0));
-        assertThat(battlefield[0][1], is(5));
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[0][0] == null);
+        assertTrue(battlefield[1][0] == null);
+        assertTrue(battlefield[2][0] == null);
+        assertTrue(battlefield[3][0] == null);
+        assertTrue(battlefield[4][0] == null);
+    }
+
+    @Test
+    public void successfullyAddedDestroyerToFieldInRowToLeft() {
+        when(random.nextInt(10)).thenReturn(3, 0);
+        when(random.nextInt(4)).thenReturn(0);
+
+        Destroyer destroyer = new Destroyer();
+        field.placeShip(destroyer);
+
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[0][0].equals(destroyer));
+        assertTrue(battlefield[1][0].equals(destroyer));
+        assertTrue(battlefield[2][0].equals(destroyer));
+        assertTrue(battlefield[3][0].equals(destroyer));
+    }
+
+    @Test
+    public void addingDestroyerToFieldInRowToLeftFailed() {
+        when(random.nextInt(10)).thenReturn(2, 0, 3, 1);
+        when(random.nextInt(4)).thenReturn(0);
+
+        Destroyer destroyer = new Destroyer();
+        field.placeShip(destroyer);
+
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[1][0] == null);
+        assertTrue(battlefield[2][0] == null);
+        assertTrue(battlefield[3][0] == null);
+        assertTrue(battlefield[4][0] == null);
     }
 
     @Test
@@ -62,14 +105,15 @@ public class FieldTest {
         when(random.nextInt(10)).thenReturn(5, 0);
         when(random.nextInt(4)).thenReturn(1);
 
-        field.placeShip(5);
+        Battleship battleship = new Battleship();
+        field.placeShip(battleship);
 
-        int[][] battlefield = field.getField();
-        assertThat(battlefield[5][0], is(5));
-        assertThat(battlefield[6][0], is(5));
-        assertThat(battlefield[7][0], is(5));
-        assertThat(battlefield[8][0], is(5));
-        assertThat(battlefield[9][0], is(5));
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[5][0].equals(battleship));
+        assertTrue(battlefield[6][0].equals(battleship));
+        assertTrue(battlefield[7][0].equals(battleship));
+        assertTrue(battlefield[8][0].equals(battleship));
+        assertTrue(battlefield[9][0].equals(battleship));
     }
 
     @Test
@@ -77,11 +121,43 @@ public class FieldTest {
         when(random.nextInt(10)).thenReturn(6, 0, 5, 1);
         when(random.nextInt(4)).thenReturn(1, 1);
 
-        field.placeShip(5);
+        Battleship battleship = new Battleship();
+        field.placeShip(battleship);
 
-        int[][] battlefield = field.getField();
-        assertThat(battlefield[5][0], is(0));
-        assertThat(battlefield[5][1], is(5));
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[6][0] == null);
+        assertTrue(battlefield[7][0] == null);
+        assertTrue(battlefield[8][0] == null);
+        assertTrue(battlefield[9][0] == null);
+    }
+
+    @Test
+    public void successfullyAddedDestroyerToFieldInRowToRight() {
+        when(random.nextInt(10)).thenReturn(6, 0);
+        when(random.nextInt(4)).thenReturn(1);
+
+        Destroyer destroyer = new Destroyer();
+        field.placeShip(destroyer);
+
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[6][0].equals(destroyer));
+        assertTrue(battlefield[7][0].equals(destroyer));
+        assertTrue(battlefield[8][0].equals(destroyer));
+        assertTrue(battlefield[9][0].equals(destroyer));
+    }
+
+    @Test
+    public void addingDestroyerToFieldInRowToRightFailed() {
+        when(random.nextInt(10)).thenReturn(7, 0, 6, 1);
+        when(random.nextInt(4)).thenReturn(1, 1);
+
+        Destroyer destroyer = new Destroyer();
+        field.placeShip(destroyer);
+
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[7][0] == null);
+        assertTrue(battlefield[8][0] == null);
+        assertTrue(battlefield[9][0] == null);
     }
 
     @Test
@@ -89,14 +165,15 @@ public class FieldTest {
         when(random.nextInt(10)).thenReturn(0, 4);
         when(random.nextInt(4)).thenReturn(3);
 
-        field.placeShip(5);
+        Battleship battleship = new Battleship();
+        field.placeShip(battleship);
 
-        int[][] battlefield = field.getField();
-        assertThat(battlefield[0][0], is(5));
-        assertThat(battlefield[0][1], is(5));
-        assertThat(battlefield[0][2], is(5));
-        assertThat(battlefield[0][3], is(5));
-        assertThat(battlefield[0][4], is(5));
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[0][0].equals(battleship));
+        assertTrue(battlefield[0][1].equals(battleship));
+        assertTrue(battlefield[0][2].equals(battleship));
+        assertTrue(battlefield[0][3].equals(battleship));
+        assertTrue(battlefield[0][4].equals(battleship));
     }
 
     @Test
@@ -104,11 +181,43 @@ public class FieldTest {
         when(random.nextInt(10)).thenReturn(0, 3, 1, 4);
         when(random.nextInt(4)).thenReturn(3);
 
-        field.placeShip(5);
+        Battleship battleship = new Battleship();
+        field.placeShip(battleship);
 
-        int[][] battlefield = field.getField();
-        assertThat(battlefield[0][0], is(0));
-        assertThat(battlefield[1][0], is(5));
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[0][0] == null);
+        assertTrue(battlefield[0][1] == null);
+        assertTrue(battlefield[0][2] == null);
+        assertTrue(battlefield[0][3] == null);
+    }
+
+    @Test
+    public void successfullyAddedDestroyerToFieldInColumnUpwards() {
+        when(random.nextInt(10)).thenReturn(0, 3);
+        when(random.nextInt(4)).thenReturn(3);
+
+        Destroyer destroyer = new Destroyer();
+        field.placeShip(destroyer);
+
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[0][0].equals(destroyer));
+        assertTrue(battlefield[0][1].equals(destroyer));
+        assertTrue(battlefield[0][2].equals(destroyer));
+        assertTrue(battlefield[0][3].equals(destroyer));
+    }
+
+    @Test
+    public void addingDestroyerToFieldInColumnUpwardsFailed() {
+        when(random.nextInt(10)).thenReturn(0, 2, 1, 3);
+        when(random.nextInt(4)).thenReturn(3);
+
+        Destroyer destroyer = new Destroyer();
+        field.placeShip(destroyer);
+
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[0][0] == null);
+        assertTrue(battlefield[0][1] == null);
+        assertTrue(battlefield[0][2] == null);
     }
 
     @Test
@@ -116,14 +225,15 @@ public class FieldTest {
         when(random.nextInt(10)).thenReturn(0, 5);
         when(random.nextInt(4)).thenReturn(2);
 
-        field.placeShip(5);
+        Battleship battleship = new Battleship();
+        field.placeShip(battleship);
 
-        int[][] battlefield = field.getField();
-        assertThat(battlefield[0][5], is(5));
-        assertThat(battlefield[0][6], is(5));
-        assertThat(battlefield[0][7], is(5));
-        assertThat(battlefield[0][8], is(5));
-        assertThat(battlefield[0][9], is(5));
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[0][5].equals(battleship));
+        assertTrue(battlefield[0][6].equals(battleship));
+        assertTrue(battlefield[0][7].equals(battleship));
+        assertTrue(battlefield[0][8].equals(battleship));
+        assertTrue(battlefield[0][9].equals(battleship));
     }
 
     @Test
@@ -131,21 +241,139 @@ public class FieldTest {
         when(random.nextInt(10)).thenReturn(0, 6, 1, 5);
         when(random.nextInt(4)).thenReturn(2);
 
-        field.placeShip(5);
+        Battleship battleship = new Battleship();
+        field.placeShip(battleship);
 
-        int[][] battlefield = field.getField();
-        assertThat(battlefield[0][9], is(0));
-        assertThat(battlefield[1][9], is(5));
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[0][6] == null);
+        assertTrue(battlefield[0][7] == null);
+        assertTrue(battlefield[0][8] == null);
+        assertTrue(battlefield[0][9] == null);
     }
 
     @Test
-    public void successfullyPerformedAttack() {
-        assertTrue(field.attack("A5"));
+    public void successfullyAddedDestroyerToFieldInColumnDownwards() {
+        when(random.nextInt(10)).thenReturn(0, 6);
+        when(random.nextInt(4)).thenReturn(2);
+
+        Destroyer destroyer = new Destroyer();
+        field.placeShip(destroyer);
+
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[0][6].equals(destroyer));
+        assertTrue(battlefield[0][7].equals(destroyer));
+        assertTrue(battlefield[0][8].equals(destroyer));
+        assertTrue(battlefield[0][9].equals(destroyer));
     }
 
     @Test
-    public void battleIsOverWhenAllShipsDestroyed() {
+    public void addingDestroyerToFieldInColumnDownwardsFailed() {
+        when(random.nextInt(10)).thenReturn(0, 7, 1, 6);
+        when(random.nextInt(4)).thenReturn(2);
+
+        Destroyer destroyer = new Destroyer();
+        field.placeShip(destroyer);
+
+        Ship[][] battlefield = field.getField();
+        assertTrue(battlefield[0][7] == null);
+        assertTrue(battlefield[0][8] == null);
+        assertTrue(battlefield[0][9] == null);
+    }
+
+    @Test
+    public void successfullyPerformedAttackOnBattleship() {
+        successfullyAddedBattleshipToFieldInRowToLeft();
+
+        assertTrue(field.attack(new int[]{0, 0}));
+        assertTrue(field.attack(new int[]{1, 0}));
+        assertTrue(field.attack(new int[]{2, 0}));
+        assertTrue(field.attack(new int[]{3, 0}));
+        assertTrue(field.attack(new int[]{4, 0}));
+    }
+
+    @Test
+    public void successfullyPerformedAttackOnDestroyer() {
+        successfullyAddedDestroyerToFieldInRowToLeft();
+
+        assertTrue(field.attack(new int[]{0, 0}));
+        assertTrue(field.attack(new int[]{1, 0}));
+        assertTrue(field.attack(new int[]{2, 0}));
+        assertTrue(field.attack(new int[]{3, 0}));
+    }
+
+    @Test
+    public void attackWasMissAsThereAreNoShipsAtGivenCoordinates() {
+        assertFalse(field.attack(new int[]{0, 0}));
+    }
+
+    @Test
+    public void afterSuccessfulAttackBattleshipSunk() {
+        successfullyPerformedAttackOnBattleship();
+        assertTrue(field.checkIfShipSunk(new int[]{0, 0}));
+    }
+
+    @Test
+    public void afterSuccessfulAttackDestroyerSunk() {
+        successfullyPerformedAttackOnBattleship();
+        assertTrue(field.checkIfShipSunk(new int[]{0, 0}));
+    }
+
+    @Test
+    public void battleIsOverWhenThereAreNoShipsOnField() {
         assertTrue(field.battleIsOver());
     }
 
+    @Test
+    public void checkForValidBattleshipCoordinatesReturnTrueIfValidCoordinates() {
+        Battleship battleship = new Battleship();
+        battleship.addCoordinates(0, 0);
+
+        assertTrue(battleship.validCoordinates(0, 0));
+    }
+
+    @Test
+    public void checkForValidDestroyerCoordinatesReturnTrueIfValidCoordinates() {
+        Destroyer destroyer = new Destroyer();
+        destroyer.addCoordinates(0, 0);
+
+        assertTrue(destroyer.validCoordinates(0, 0));
+    }
+
+    @Test
+    public void checkForValidBattleshipCoordinatesReturnFalseWhenInvalidCoordinates() {
+        Battleship battleship = new Battleship();
+        battleship.addCoordinates(1, 1);
+
+        assertFalse(battleship.validCoordinates(0, 0));
+    }
+
+    @Test
+    public void checkForValidDestroyerCoordinatesReturnFalseWhenInvalidCoordinates() {
+        Destroyer destroyer = new Destroyer();
+        destroyer.addCoordinates(1, 1);
+
+        assertFalse(destroyer.validCoordinates(0, 0));
+    }
+
+    @Test
+    public void whenNoCoordinatesLeftForBattleshipItHasSunk() {
+        Battleship battleship = new Battleship();
+
+        assertTrue(battleship.isSunk());
+    }
+
+    @Test
+    public void whenNoCoordinatesLeftForDestroyerItHasSunk() {
+        Destroyer destroyer = new Destroyer();
+
+        assertTrue(destroyer.isSunk());
+    }
+
+    @Test
+    public void coordinatesAreCorrectlyTranslatedFromString() {
+        int[] coordinates = field.getCoordinates("A1");
+
+        assertThat(coordinates[0], is(0));
+        assertThat(coordinates[1], is(0));
+    }
 }
