ae804c77fd209d725e9c4860593572c855170c7f
Martins <martins.git@irbe.co.uk>
2017-04-30 19:30:49 +0100

    Removing unused import.

--- src/test/java/battleships/logic/FieldTest.java
+++ src/test/java/battleships/logic/FieldTest.java
@@ -9,7 +9,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.Map;
 import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.is;
